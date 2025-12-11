e8847d2a6f326fb7a9e525d52f3f76cb2d98ca6a
princesslotte <lottew831@gmail.com>
2025-12-12 02:21:35 +0800

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANT: The repository name on GitHub MUST be 'yiwuhangzhou2025trip'
-  base: '/yiwuhangzhou2025trip/',
-});
